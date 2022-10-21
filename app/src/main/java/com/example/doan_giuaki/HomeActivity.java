c4e5735b736726b7fae3b617c69acbb86ffa0d95
kngansokewt <tranngan55599@gmail.com>
2022-10-21 23:48:50 +0700

    App Tong Hop

---
+++ app/src/main/java/com/example/doan_giuaki/HomeActivity.java
@@ -0,0 +1,38 @@
+package com.example.doan_giuaki;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+
+    Button btnlistview, profile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        btnlistview = (Button) findViewById(R.id.btnlistview);
+        profile = (Button) findViewById(R.id.profile);
+
+        btnlistview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mh1 = new Intent(HomeActivity.this, ListviewActivity.class);
+                startActivity(mh1);
+            }
+        });
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Intent mh2 = new Intent(HomeActivity.this, UserProfileActivity.class);
+               startActivity(mh2);
+            }
+        });
+    }
+}
